3ebbeb23f4e2c22ccb25ea7273bfc4e0a2fbe8e7
bjk1649 <qhwns1649@naver.com>
2022-12-21 22:54:24 +0900

    fix: 로직수정

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ COPY . /app
 WORKDIR /app
 RUN pip install Flask
 RUN pip install requests
+RUN apt-get update && apt-get install curl -y
 ENTRYPOINT [ "python" ]
 CMD [ "app.py" ]
--- app.py
+++ app.py
@@ -1,44 +1,66 @@
-from flask import Flask
-from flask import request
-app = Flask(__name__)
-import requests
+import logging
+import os
 import sys
+
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
 @app.route('/')
 def start():
     return 'Hello i am a Health Check Container!\n /ping <---- you can check! \n'
 
-@app.route('/ping')
-def do_ping():
-    error = ' '
-    response1 = ' '
-    response2 = ' '
-    response3 = ' '
-    try:
-        response1 = requests.get('http://member:4001/pong')
-        
-    except requests.exceptions.RequestException as e:
-        print('Cannot reach the memeber container.\n', file=sys.stderr)
-        error = requests.get('http://resource:4004/error?con=M')
-        return 'M_ERROR\n'
-    
-    try:
-        response2 = requests.get('http://product:4002/pong', file=sys.stderr)
-        
-    except requests.exceptions.RequestException as e:
-        print('Cannot reach the product container.\n')
-        error = requests.get('http://resource:4004/error?con=P')
-        return 'P_ERROR\n'
 
-    try:
-        response3 = requests.get('http://inquiry:4003/pong', file=sys.stderr)
-        
-    except requests.exceptions.RequestException as e:
-        print('Cannot reach the inquiry container.\n')
-        error = requests.get('http://resource:4004/error?con=I')
-        return 'I_ERROR\n'
+@app.route('/startCheck')
+def checkContainers():
+    memberNoAckNum = 0
+    productNoAckNum = 0
+    gatewayNoAckNum = 0
+
+    while True:
+        try:
+            response1 = requests.get('http://member:4001/ping')
+        except requests.exceptions.RequestException as e:
+            memberNoAckNum += 1
+
+        try:
+            response2 = requests.get(
+                'http://product:4002/ping', file=sys.stderr)
+
+        except requests.exceptions.RequestException as e:
+            productNoAckNum += 1
+
+        try:
+            response3 = requests.get(
+                'http://gateway:8080/ping', file=sys.stderr)
+
+        except requests.exceptions.RequestException as e:
+            gatewayNoAckNum += 1
+
+        if memberNoAckNum > 3:
+            logging.warning('restart member container')
+            os.system("curl host.docker.internal:8080/error/member")
+            memberNoAckNum = 0
+        if productNoAckNum > 3:
+            logging.warning('restart product container')
+            os.system("curl host.docker.internal:8080/error/product")
+            productNoAckNum = 0
+        if gatewayNoAckNum > 3:
+            logging.warning('restart gateway container')
+            os.system("curl host.docker.internal:8080/error/gateway")
+            gatewayNoAckNum = 0
+        sleep(1)
+        print('NO ERROR \n', file=sys.stderr)
+        return 'NO_ERROR\n'
 
 
-    print('NO ERROR \n',file=sys.stderr)
-    return 'NO_ERROR\n'
+@app.route('/test')
+def test():
+    os.system("curl host.docker.internal:8080/v1/users/test")
+    return 'test'
+
+
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', port = 4000, debug = True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
