c63339a740c10c22641c8ab36e197596dc5d9a73
bjk1649 <qhwns1649@naver.com>
2022-12-22 01:58:28 +0900

    feat: product db 23마다 동기화기능추가

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import logging
 import os
 import sys
+import time
+from sched import scheduler
 
 import requests
 from flask import Flask, request
 
+scheduler.every().day.at("23:00").do(requests.get('http://product:4002/sync'))
 app = Flask(__name__)
 
 
@@ -15,45 +18,54 @@ def start():
 
 @app.route('/startCheck')
 def checkContainers():
+    global schedule
     memberNoAckNum = 0
     productNoAckNum = 0
     gatewayNoAckNum = 0
 
     while True:
+        schedule.run_pending()
         try:
-            response1 = requests.get('http://member:4001/ping')
+            response1 = requests.get('http://member:4001/ping', timeout=2)
+            if response1.text != "OK":
+                memberNoAckNum += 1
+            print("member: ", response1.text)
         except requests.exceptions.RequestException as e:
             memberNoAckNum += 1
 
         try:
             response2 = requests.get(
-                'http://product:4002/ping', file=sys.stderr)
+                'http://product:4002/ping', timeout=2)
+            if response2.text != "OK":
+                productNoAckNum += 1
+            print("product: ", response2.text)
 
         except requests.exceptions.RequestException as e:
             productNoAckNum += 1
 
         try:
             response3 = requests.get(
-                'http://gateway:8080/ping', file=sys.stderr)
+                'http://gateway:8080/ping', timeout=2)
+            if response3.text != "OK":
+                gatewayNoAckNum += 1
+            print("gateway: ", response3.text)
 
         except requests.exceptions.RequestException as e:
             gatewayNoAckNum += 1
 
         if memberNoAckNum > 3:
             logging.warning('restart member container')
-            os.system("curl host.docker.internal:8080/error/member")
+            os.system("curl host.docker.internal:4050/error/member")
             memberNoAckNum = 0
         if productNoAckNum > 3:
             logging.warning('restart product container')
-            os.system("curl host.docker.internal:8080/error/product")
+            os.system("curl host.docker.internal:4050/error/product")
             productNoAckNum = 0
         if gatewayNoAckNum > 3:
             logging.warning('restart gateway container')
-            os.system("curl host.docker.internal:8080/error/gateway")
+            os.system("curl host.docker.internal:4050/error/gateway")
             gatewayNoAckNum = 0
-        sleep(1)
-        print('NO ERROR \n', file=sys.stderr)
-        return 'NO_ERROR\n'
+        time.sleep(1)
 
 
 @app.route('/test')
